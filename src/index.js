57d1661e1e10c98ff0e66c0f886a5f2ff8d66842
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-25 21:33:00 +0600

    add parameter in function

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 const index = 0;
 
-function Clock() {
+function Clock({ locale }) {
     return (
         <h1 className="heading" tabIndex={index}>
-            <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+            <span className="text">Hello {new Date().toLocaleTimeString(locale)}</span>
             <img src="" alt="" />
         </h1>
     );
 }
-ReactDOM.render(<Clock />, document.getElementById('root'));
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
