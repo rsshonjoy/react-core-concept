dc3f7d83ae8a9da6a637f2e47f1e61ca5a350203
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-25 21:29:58 +0600

    component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const element = React.createElement('h1', null, 'Hello world');
-
 const index = 0;
 
-setInterval(() => {
-    const element = (
+function Clock() {
+    return (
         <h1 className="heading" tabIndex={index}>
             <span className="text">Hello {new Date().toLocaleTimeString()}</span>
             <img src="" alt="" />
         </h1>
     );
-    ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+}
+ReactDOM.render(Clock(), document.getElementById('root'));
