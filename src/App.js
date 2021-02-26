17cbc3d5a3f391b10a1b6a5f5de8262273f358c8
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 16:02:46 +0600

    use map

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Card from './components/Card';
 import Data from './data.json';
 
 function App() {
-    const items = [];
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < Data.length; i++) {
-        items.push(<Card titleText={Data[i].title} descText={Data[i].desc} />);
-    }
+    let items = [];
+    items = Data.map((item) => <Card titleText={item.title} descText={item.desc} />);
     return (
         <div>
             <h1 className="headingStyle">Todo App</h1>
