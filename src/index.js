40a2a365926d548ee72fa8a053cd99d4566a56e2
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 13:58:48 +0600

    card design

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const todoTitle = 'Call Family';
-const todoDesc = 'rs shonjoy is a full stark web developer.';
+const todoDesc = "My name is Shonjoy Das. I'am a full stark web developer.";
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
@@ -11,9 +11,12 @@ const currentYear = date.getFullYear();
 
 ReactDOM.render(
     <div>
-        <h1 className="headingStyle">{todoTitle}</h1>
-        <h3>{todoDesc}</h3>
-        <p>{`${dateName} ${monthName} ${currentYear}`}</p>
+        <h1 className="headingStyle">Todo App</h1>
+        <div className="card">
+            <h3 className="cardTitle">{todoTitle}</h3>
+            <p className="cardDesc">{todoDesc}</p>
+            <p className="cardFooter">{`${dateName} ${monthName} ${currentYear}`}</p>
+        </div>
     </div>,
     document.getElementById('root')
 );
