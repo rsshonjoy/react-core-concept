30419771bebfb48ababc55e7e6b4a29621499571
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 14:10:48 +0600

    external component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './Card';
 import './index.css';
-
-const todoTitle = 'Call Family';
-const todoDesc = "My name is Shonjoy Das. I'am a full stark web developer.";
-const date = new Date();
-const dateName = date.getDate();
-const monthName = date.getMonth();
-const currentYear = date.getFullYear();
-
-function Card() {
-    return (
-        <div className="card">
-            <h3 className="cardTitle">{todoTitle}</h3>
-            <p className="cardDesc">{todoDesc}</p>
-            <p className="cardFooter">{`${dateName} ${monthName} ${currentYear}`}</p>
-        </div>
-    );
-}
 
 ReactDOM.render(
     <div>
