8a26443eb4d60343c574f9007c31cefba08ae88e
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 16:04:39 +0600

    map shortcut

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Card from './components/Card';
 import Data from './data.json';
 
 function App() {
-    let items = [];
-    items = Data.map((item) => <Card titleText={item.title} descText={item.desc} />);
     return (
         <div>
             <h1 className="headingStyle">Todo App</h1>
-            {items}
+            {Data.map((item) => (
+                <Card titleText={item.title} descText={item.desc} />
+            ))}
         </div>
     );
 }
