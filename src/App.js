aa2bf6b75783df54fc6b12d09e6b4c13f79b3f59
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 15:54:55 +0600

    use for loop

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Card from './components/Card';
 import Data from './data.json';
 
 function App() {
+    const items = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < Data.length; i++) {
+        items.push(<Card titleText={Data[i].title} descText={Data[i].desc} />);
+    }
     return (
         <div>
             <h1 className="headingStyle">Todo App</h1>
-            <Card titleText={Data[0].title} descText={Data[0].desc} />
-            <Card titleText={Data[1].title} descText={Data[1].desc} />
-            <Card titleText={Data[2].title} descText={Data[2].desc} />
-            <Card titleText={Data[3].title} descText={Data[3].desc} />
+            {items}
         </div>
     );
 }
