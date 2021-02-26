64ef718f773f0cc6f657c6aab58ae019c0790c36
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 10:51:04 +0600

    date dinamic change

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 const todoTitle = 'Call Family';
 const todoDesc = 'rs shonjoy is a full stark web developer.';
-const date = '26/02/2021';
+// const date = '26/02/2021';
 
 ReactDOM.render(
     <div>
         <h1>{todoTitle}</h1>
         <h3>{todoDesc}</h3>
-        <p>{date}</p>
+        <p>{new Date().getFullYear()}</p>
     </div>,
     document.getElementById('root')
 );
