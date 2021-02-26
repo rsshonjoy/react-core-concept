72ede8d9f485aa9ff5cec3ae412224fd11828ebc
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 13:39:05 +0600

    add file in sorce folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const todoTitle = 'Call Family';
 const todoDesc = 'rs shonjoy is a full stark web developer.';
