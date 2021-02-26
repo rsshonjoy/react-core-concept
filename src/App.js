63a605502724b572ac7a3ef277243cf97c70593e
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 15:26:04 +0600

    create and load data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import Card from './components/Card';
+import Data from './data.json';
 
 function App() {
     return (
         <div>
             <h1 className="headingStyle">Todo App</h1>
-            <Card
-                titleText="Call Family"
-                descText="My name is Shonjoy Das. I'am a full stark web developer."
-            />
-            <Card
-                titleText="Call Family"
-                descText="My name is Shonjoy Das. I'am a full stark web developer."
-            />
-            <Card
-                titleText="Call Family"
-                descText="My name is Shonjoy Das. I'am a full stark web developer."
-            />
-            <Card
-                titleText="Call Family"
-                descText="My name is Shonjoy Das. I'am a full stark web developer."
-            />
+            <Card titleText={Data[0].title} descText={Data[0].desc} />
+            <Card titleText={Data[1].title} descText={Data[1].desc} />
+            <Card titleText={Data[2].title} descText={Data[2].desc} />
+            <Card titleText={Data[3].title} descText={Data[3].desc} />
         </div>
     );
 }
