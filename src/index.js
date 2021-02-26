9c2794db4311de2848d2d5786451b50b75f01061
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 14:20:08 +0600

    use external component folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(
-    <div>
-        <h1 className="headingStyle">Todo App</h1>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-    </div>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
